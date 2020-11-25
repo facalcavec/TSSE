6ee2fcbe027f8574f149f3afc015563ff95273ef
facalcavec <facalcavec@gmail.com>
2020-11-25 15:31:17 -0300

    TP1 version 1.0

---
+++ TP1/leds/src/leds.h
@@ -0,0 +1,8 @@
+#include <stdint.h>
+
+void Leds_Create(uint16_t *puerto);
+void Leds_On( uint8_t );
+void Leds_Off( uint8_t );
+void Led_TurnAllOn(void);
+void Led_TurnAllOff(void);
+uint8_t Estado_de_un_Led (uint8_t led);
